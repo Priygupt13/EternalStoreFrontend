16c3e166868b4613dda2ca1935fc48c25e976f5f
Priya Gupta <er.priyagupta123@gmail.com>
2022-10-12 16:31:45 -0700

    Use prod backend

--- src/common/const.js
+++ src/common/const.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.REACT_APP_USE_PROD_BACKEND && process.env.REACT_APP_USE_PROD_BACKEND == "true" 
-                        ?  "https://24tly05lg0.execute-api.us-west-1.amazonaws.com/Prod/api"
-                        : "http://localhost:5000/api";
+export const API_URL = "https://24tly05lg0.execute-api.us-west-1.amazonaws.com/Prod/api";
